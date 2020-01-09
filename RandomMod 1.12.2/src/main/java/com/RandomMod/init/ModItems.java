ad632f405ca551498a3b2626e4bd57ba927258d0
A.bock <A.bock@RHS193-2002266.rhs193.org>
2020-01-09 13:02:43 -0500

    ITEM

--- RandomMod 1.12.2/src/main/java/com/RandomMod/init/ModItems.java
+++ RandomMod 1.12.2/src/main/java/com/RandomMod/init/ModItems.java
@@ -11,7 +11,7 @@ public class ModItems
 {
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
-	public static final Item BARNABIC_INGOT= new ItemBase("barnabic_ingot");
+	public static final Item BARNABIC_INGOT= new ItemBase("barnabicingot");
 	
 
 }
