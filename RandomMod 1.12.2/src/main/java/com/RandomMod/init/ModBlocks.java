4fc27945ce3d61524b5e7ef6d6d9f52a943c6027
KitKatTheKandy <kmorhun@gmail.com>
2020-01-14 12:13:54 -0500

    Block models+textures added

--- RandomMod 1.12.2/src/main/java/com/RandomMod/init/ModBlocks.java
+++ RandomMod 1.12.2/src/main/java/com/RandomMod/init/ModBlocks.java
@@ -10,6 +10,8 @@ import net.minecraft.block.material.Material;
 
 public class ModBlocks 
 {
+	//to add a new item: add to this file, add a model, add a texture, and add it to the lang file
+	
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
 	public static final Block BARNABIC_ORE = new BlockBase("barnabic_ore", Material.ROCK);
