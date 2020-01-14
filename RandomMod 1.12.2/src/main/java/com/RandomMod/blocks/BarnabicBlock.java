2a3881df371c5dce2e3a9d14680d796ff888db50
KitKatTheKandy <kmorhun@gmail.com>
2020-01-14 12:26:48 -0500

    block properties for Barnabic Block and Cheese

---
+++ RandomMod 1.12.2/src/main/java/com/RandomMod/blocks/BarnabicBlock.java
@@ -0,0 +1,19 @@
+package com.RandomMod.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class BarnabicBlock extends BlockBase
+{
+
+	public BarnabicBlock(String name, Material material) 
+	{
+		super(name, material);
+		setSoundType(SoundType.METAL); //sound while walking over
+		setHardness(5.0f); //how long it takes to break
+		setResistance(15.0f); //how resistant to explosives
+		setHarvestLevel("pickaxe", 2); // 0-3 wood to diamond
+		setLightLevel(1.0f); //emit light?
+	}
+
+}
