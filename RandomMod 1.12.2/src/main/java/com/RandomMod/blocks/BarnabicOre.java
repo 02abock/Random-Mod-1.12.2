7c7f4bebdf1cb6517f05d18bdb0623c8f6b86d82
a.Bock <a.Bock@RHS193-2002266.rhs193.org>
2020-01-15 12:51:31 -0500

    ore

---
+++ RandomMod 1.12.2/src/main/java/com/RandomMod/blocks/BarnabicOre.java
@@ -0,0 +1,18 @@
+package com.RandomMod.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class BarnabicOre extends BlockBase
+{
+	public BarnabicOre(String name, Material material) 
+	{
+		super(name, material);
+		setSoundType(SoundType.METAL); //sound while walking over
+		setHardness(5.0f); //how long it takes to break
+		setResistance(15.0f); //how resistant to explosives
+		setHarvestLevel("pickaxe", 3); // 0-3 wood to diamond
+		setLightLevel(1.0f); //emit light?
+	}
+
+}
