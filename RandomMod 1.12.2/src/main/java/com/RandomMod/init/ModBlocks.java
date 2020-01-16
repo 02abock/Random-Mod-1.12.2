802d2946802af2f6f396a9aaeb72db3a6e22ce5e
KitKatTheKandy <kmorhun@gmail.com>
2020-01-16 14:28:00 -0500

    fixed cheese+barnabic ore

--- RandomMod 1.12.2/src/main/java/com/RandomMod/init/ModBlocks.java
+++ RandomMod 1.12.2/src/main/java/com/RandomMod/init/ModBlocks.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.RandomMod.blocks.BarnabicBlock;
-import com.RandomMod.blocks.BlockBase;
+import com.RandomMod.blocks.BarnabicOre;
+import com.RandomMod.blocks.CheeseBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -15,7 +16,7 @@ public class ModBlocks
 	
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
-	public static final Block BARNABIC_ORE = new BlockBase("barnabic_ore", Material.ROCK);
+	public static final Block BARNABIC_ORE = new BarnabicOre("barnabic_ore", Material.ROCK);
 	public static final Block BARNABIC_BLOCK = new BarnabicBlock("barnabic_block", Material.IRON);
-	public static final Block CHEESE_BLOCK = new BlockBase("cheese_block", Material.CAKE);
+	public static final Block CHEESE_BLOCK = new CheeseBlock("cheese_block", Material.CAKE);
 }
